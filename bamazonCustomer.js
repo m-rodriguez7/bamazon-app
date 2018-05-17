8b20067b73ee8f9c5ff3eb36436cb379877ecf52
m-rodriguez7 <michael.rodriguez7@icloud.com>
2018-05-17 15:33:25 -0700

    thats enough

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer")
 var mysql = require("mysql");
-
+var stock;
+// i was thinking about adding a npm table but no time
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,42 +12,88 @@ var connection = mysql.createConnection({
     // Your password
     password: "",
     database: "bamazon_db"
-  });
+});
 
-  connection.connect(function(err) {
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    readProducts(); // need to fix this so its ~prettier
-    inquirer.prompt([
-    {
-      type: "input",
-      message: "Choose product id for purchase:",
-      name: "product_id"
-    },
-    {
-      type: "input",
-      message: "How many of this product would you like to buy?",
-      name: "amount"
-    }
-  ])
-  .then(function(inquirerResponse) {
-    // sql calls/checks here. there will eventually be a whole CRUD process
-    console.log("checking item stock...");
+    // Log all results of the SELECT statement
+    console.log(res);
   });
-    //readProducts()
-  });
-  
-  
-  
-  
-  
-  function readProducts() {
-    console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-  }
+}
 
+function updateProduct(productId, amount) {
+    connection.query(
+      "UPDATE products SET stock_quantity = stock_quantity - " +amount+ " WHERE item_id = " + productId + ""
+      ,
+      function(err, res) {
+        if (err) throw err;
+        // display the price for the item as well
+        displayPrice(productId, amount);
+      }
+    );
+}
+
+function checkStock(productId) {
+  connection.query(
+    "SELECT stock_quantity FROM products WHERE item_id = " + productId,
+    function(err,res) {
+      if (err) throw err;
+      stock = res[0].stock_quantity;
+    }
+  )
+}
+
+function displayPrice(productId, amount) {
+  connection.query(
+    "SELECT price FROM products WHERE item_id = " + productId,
+    function(err,res) {
+      if (err) throw err;
+      console.log("Your total today is: $" + parseInt(res[0].price)*amount);
+      connection.end();
+    }
+  )
+}
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readProducts(); // need to fix this so its ~prettier
+  setTimeout(function() { // so inquirer displays after the items
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "Choose product id for purchase:",
+        name: "product_id"
+      },
+      {
+        type: "input",
+        message: "How many of this product would you like to buy?",
+        name: "amount"
+      }
+    ])
+    .then(function(r) {
+      // sql calls/checks here. there will eventually be a whole CRUD process
+      console.log("checking item stock...");
+      connection.query(
+        "SELECT stock_quantity FROM products WHERE item_id = " + r.product_id
+      ,
+        function(err,res) { // asynchronous calls go F urself
+          if (err) throw err;
+          stock = res[0].stock_quantity;
+          console.log(stock);
+          if (parseInt(r.amount) > parseInt(stock)) {
+            console.log("I'm sorry, we don't have enough of that item! Try again.");
+            connection.end();
+            return;
+          } else {
+            updateProduct(r.product_id, r.amount);
+          };
+        }
+      )
+      
+      
+    });
+  }, 100);
+});
